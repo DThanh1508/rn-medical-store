54b2bc90dfe60624b0714d6d9e474264b9fd707f
DThanh1508 <athanh2002kt@gmail.com>
2022-10-21 17:06:22 +0700

    Medical store

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// rnfes
+
+export default App = () => {
+  return (
+    <View>
+      <Text>HI</Text>
+    </View>
+  ); 
+};
