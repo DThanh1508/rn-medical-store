1c6c4aecb6c49e452fe1b0ad266bec78d0cc08c2
DThanh1508 <athanh2002kt@gmail.com>
2022-10-25 16:38:32 +0700

    Build profile file

---
+++ src/pages/SettingProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const SettingProfile = () => {
+    return (
+        <Text>Profile page!</Text>
+    );
+}
+
+export default SettingProfile;
