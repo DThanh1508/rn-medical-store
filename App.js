8bc54f89bb91e9fcb3a33e0e6ee416c80f1d1a09
DThanh1508 <athanh2002kt@gmail.com>
2022-11-01 16:55:00 +0700

    Run successfully

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Screens from "./src/Screens";
 import {View} from 'react-native';
-import Props from './lessons/Props';
+// import Props from './lessons/Props';
 
 // rnfes
 
 export default App = () => {
   return (
-      // <Screens/>
-      <View>
-        <Props text={'hihgsdfgd4fsgi'}/>
-      </View>
+      <Screens/>
+      // <View>
+      //   <Props text={'hihgsdfgd4fsgi'}/>
+      // </View>
   );
 };
